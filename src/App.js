21a3fb02959cd3c02ab81e1bdf82c864eef99a22
Utkarsh Mishra <utkarshmishra2022@u.northwestern.edu>
2020-01-30 23:59:40 -0600

    final slice

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 import logo from './logo.svg';
 import './App.css';
-import { Grid, Icon, Image, Header, Button, Segment, List, Card, GridRow } from 'semantic-ui-react';
+import { Grid, Icon, Modal, Image, Header, Button, Segment, List, Card, GridRow } from 'semantic-ui-react';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCC1XbBmXqTY1IbpfQv6UxwcYrChXgKNng",
@@ -30,6 +30,44 @@ function App() {
     db.on('value', handleData, error => alert(error));
     return () => { db.off('value', handleData); };
   }, []);
+  const NestedModal = () =>{
+    const [popup, setPopup] = useState(false);
+
+    return(
+      <Modal
+        open={popup}
+        onOpen={()=> setPopup(true)}
+        onClose={()=> setPopup(false)}
+        size="medium"
+        trigger={<Button color="green">Book</Button>}
+      >
+        <Modal.Content>
+          <Image
+            centered
+            wrapped
+            size="large"
+            src="https://imageog.flaticon.com/icons/png/512/1004/1004765.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF"
+          />
+
+          <Modal.Description>
+            <Grid centered padded="20">
+              <Header>
+                Success! A confirmation page will be sent to your email
+              </Header>
+            </Grid>
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            icon="check"
+            color="green"
+            content="All Done"
+            onClick={()=> setPopup(false)}
+          />
+        </Modal.Actions>
+      </Modal>
+    )
+  };
   return (
     <div>
       <Header as='h2' icon textAlign='center'>
@@ -59,16 +97,45 @@ function App() {
                         </Button.Group>
                       </Grid.Row>
                       <Grid.Row>
-                      <Button style={{textAlign: "center"}}> <strong>Background Check</strong></Button>
+                        <Modal trigger={<Button secondary> <strong>Background Check</strong></Button>} textAlign="center">
+                          <Modal.Header>{inventory.Name}</Modal.Header>
+                          <Modal.Content image>
+                            <Image
+                              wrapped
+                              size="large"
+                              src="https://react.semantic-ui.com/images/avatar/large/rachel.png"
+                            />
+                            <Modal.Description>
+                              <Header></Header>
+                              <List>
+                                <List.Header><Header as="h4">BackGround Check: </Header>{(inventory.BackgroundChecked == "Y") ? "BackGround Checked" : "Background Not Checked"}</List.Header>
+                                <List.Item><Header as="h4">Age: </Header>{inventory.Age}</List.Item>
+                                <List.Item>CPR Certified</List.Item>
+                                <List.Item>License: {(inventory.NursingLicense == "Y") ? "Licensed Nurse" : "Unlicensed Nurse"}</List.Item>
+                                <List.Item><Header as="h4">Gender: </Header>{inventory.Gender}</List.Item>
+                                <List.Item><Header as="h4">Hourly Rate: </Header>{inventory.HourlyRate}</List.Item>
+                              </List>
+                              <p>
+                                My name is {inventory.Name}, and I am an experienced senior caregiver with an
+                                extensive background as a CNA. Over the past {inventory.YearsExperience} years, I have served
+                                over 10 clients in the greater Chicago area, providing help through
+                                medication reminders, grocery shopping and meal prep, and mobility
+                                assistance. I am very patient, and work well with clients who needs
+                                extra care, such as seniors with Alzheimer’s.</p>
+                            </Modal.Description>
+                          </Modal.Content>
+                          <Modal.Actions>
+                          <NestedModal />
+                          </Modal.Actions>
+                        </Modal>
+
                       </Grid.Row>
                     </Grid>
                   </Card.Description>
                 </Card.Content>
                 <Card.Content extra>
                   <div className='ui two buttons'>
-                    <Button basic color='green'>
-                      See More
-                </Button>
+                    <Button basic color='green'>See More</Button>
                     <Button basic color='red'>
                       Decline
                 </Button>
@@ -81,7 +148,7 @@ function App() {
               <Image
                 floated='right'
                 size='medium'
-                src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
+                src='https://react.semantic-ui.com/images/avatar/large/rachel.png'
               />
             </Grid.Column>
           </Grid.Row>
