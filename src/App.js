5613afe3a7708b7c363ecce1b4a30da40db593ff
Karan Bhasin <karanbhasin2022@u.northwestern.edu>
2020-01-30 19:29:11 -0600

    first push with three cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Grid, Image, Header, Button, Segment, List, Card } from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Card.Group>
+    <Card>
+      <Card.Content>
+        <Image
+          floated='right'
+          size='mini'
+          src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
+        />
+        <Card.Header>Steve Sanders</Card.Header>
+        <Card.Meta>5 Years of Experience</Card.Meta>
+        <Card.Description>
+          <strong>Non-Smoker</strong>
+          <Button> <strong>Background Check</strong></Button>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className='ui two buttons'>
+          <Button basic color='green'>
+            See More
+          </Button>
+          <Button basic color='red'>
+            Decline
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>
+    <Card>
+      <Card.Content>
+        <Image
+          floated='right'
+          size='mini'
+          src='https://react.semantic-ui.com/images/avatar/large/molly.png'
+        />
+        <Card.Header>Molly Thomas</Card.Header>
+        <Card.Meta>New User</Card.Meta>
+        <Card.Description>
+          Molly wants to add you to the group <strong>musicians</strong>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className='ui two buttons'>
+          <Button basic color='green'>
+            Approve
+          </Button>
+          <Button basic color='red'>
+            Decline
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>
+    <Card>
+      <Card.Content>
+        <Image
+          floated='right'
+          size='mini'
+          src='https://react.semantic-ui.com/images/avatar/large/jenny.jpg'
+        />
+        <Card.Header>Jenny Lawrence</Card.Header>
+        <Card.Meta>New User</Card.Meta>
+        <Card.Description>
+          Jenny requested permission to view your contact details
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className='ui two buttons'>
+          <Button basic color='green'>
+            Approve
+          </Button>
+          <Button basic color='red'>
+            Decline
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>
+  </Card.Group>
+    
     </div>
   );
 }
